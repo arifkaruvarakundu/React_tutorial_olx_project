2cee5c79db9c45c0c61841b5a8d22e8bbc4b333a
muhammed harif <harifn786@gmail.com>
2023-09-12 19:29:08 +0530

    started to make olx

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router,Route} from 'react-router-dom'
+import signup from './Pages/Signup'
 
 /**
  * ?  =====Import Components=====
  */
 import Home from './Pages/Home';
+import Signup from './Components/Signup/Signup';
 
 function App() {
   return (
     <div>
-      <Home />
+      <Router>
+        <Route path='/'>
+        <Home />
+        </Route>
+        <Route path='/signup'>
+        <Signup />
+        </Route>
+      </Router>
+      
     </div>
   );
 }
